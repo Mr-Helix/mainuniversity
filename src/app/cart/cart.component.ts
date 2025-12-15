53d30e25b7e4fbd00836d6cf1e38c4617cf23251
Saba S <helix2849@gmail.com>
2025-12-15 20:13:52 +0400

    gasworebulia responsive, textebi damatebulia produqcia da secvlilia fontebi

--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -1,17 +1,18 @@
 import { Component } from '@angular/core';
 import { CartService } from '../cart.service';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-cart',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, RouterModule],
   templateUrl: './cart.component.html',
   styleUrl: './cart.component.css'
 })
 export class CartComponent {
   constructor(public cartService: CartService) {}
-  
+
   get total() {
   return this.cartService.getTotal().toFixed(2);
 }
